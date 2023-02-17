42ef734765d8dc2e3ea966a5c4a99b47e713198d
v.yaremchuk@admin.idknet.com <v.yaremchuk@admin.idknet.com>
2023-02-17 14:20:17 +0200

    Add loading indicator after scan

--- lib/menus/water_menu.dart
+++ lib/menus/water_menu.dart
@@ -1,23 +1,38 @@
 import 'package:com_pay/routes/scanner_poute.dart';
 import 'package:com_pay/routes/water_routes/my_water_meters_route.dart';
 import 'package:com_pay/widgets/menu_item.dart';
+import 'package:com_pay/widgets/overlay_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:com_pay/api.dart' as api;
 
 import '../routes/water_routes/search_water_meter_route.dart';
 import '../routes/water_routes/water_meter_route.dart';
+import '../widgets/loading_indicator.dart';
 
-class WaterMenu extends StatelessWidget {
+class WaterMenu extends StatefulWidget {
   final String keyString;
   const WaterMenu({super.key, required this.keyString});
+
+  @override
+  State<StatefulWidget> createState() => _WaterMenuState();
+}
+
+class _WaterMenuState extends State<WaterMenu> {
+  bool loading = false;
+
+  void _setLoading(bool value) {
+    setState(() {
+      loading = value;
+    });
+  }
 
   void _goToMeters(BuildContext context) {
     Navigator.push(
         context,
         MaterialPageRoute(
             builder: (_) => MyWaterMetersRoute(
-                  keyString: keyString,
+                  keyString: widget.keyString,
                 )));
   }
 
@@ -25,13 +40,15 @@ class WaterMenu extends StatelessWidget {
     String? result = await Navigator.push(
         context, MaterialPageRoute(builder: (ctx) => const ScannerRoute()));
     if (result == null) return;
-    api.searchWaterMeter(keyString, result).then((list) {
+    _setLoading(true);
+    api.searchWaterMeter(widget.keyString, result).then((list) {
+      _setLoading(false);
       if (list.isEmpty) return;
       Navigator.push(
           context,
           MaterialPageRoute(
-              builder: (ctx) =>
-                  WaterMeterRoute(keyString: keyString, meter: list[0])));
+              builder: (ctx) => WaterMeterRoute(
+                  keyString: widget.keyString, meter: list[0])));
     });
   }
 
@@ -40,28 +57,39 @@ class WaterMenu extends StatelessWidget {
         context,
         MaterialPageRoute(
             builder: (_) => SearchWaterMeterRoute(
-                  keyString: keyString,
+                  keyString: widget.keyString,
                 )));
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        MenuItem(
-            icon: Icons.speed,
-            text: AppLocalizations.of(context)!.myWaterMeters,
-            onTap: () => _goToMeters(context)),
-        MenuItem(
-            icon: Icons.qr_code,
-            text: AppLocalizations.of(context)!.searchWaterQr,
-            onTap: () => _goToScanner(context)),
-        MenuItem(
-          icon: Icons.search,
-          text: AppLocalizations.of(context)!.searchWaterCode,
-          onTap: () => _goToSearch(context),
-        )
-      ],
+    return OverlayWidget(
+      overlay: loading
+          ? Container(
+              color: Colors.black.withAlpha(150),
+              alignment: Alignment.center,
+              width: double.maxFinite,
+              height: double.maxFinite,
+              child: const LoadingIndicator(),
+            )
+          : Container(),
+      child: ListView(
+        children: [
+          MenuItem(
+              icon: Icons.speed,
+              text: AppLocalizations.of(context)!.myWaterMeters,
+              onTap: () => _goToMeters(context)),
+          MenuItem(
+              icon: Icons.qr_code,
+              text: AppLocalizations.of(context)!.searchWaterQr,
+              onTap: () => _goToScanner(context)),
+          MenuItem(
+            icon: Icons.search,
+            text: AppLocalizations.of(context)!.searchWaterCode,
+            onTap: () => _goToSearch(context),
+          )
+        ],
+      ),
     );
   }
 }
