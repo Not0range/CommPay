9a44fa96886191483fd5386267243bc048b45bb5
v.yaremchuk@admin.idknet.com <v.yaremchuk@admin.idknet.com>
2023-02-21 13:39:23 +0200

    Change menu item

--- lib/widgets/menu_item.dart
+++ lib/widgets/menu_item.dart
@@ -8,33 +8,12 @@ class MenuItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 16),
-          decoration: const BoxDecoration(
-              border: Border(
-                  left:
-                      BorderSide(style: BorderStyle.solid, color: Colors.black),
-                  bottom:
-                      BorderSide(style: BorderStyle.solid, color: Colors.black),
-                  right: BorderSide(
-                      style: BorderStyle.solid, color: Colors.black))),
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 4),
-                child: Icon(icon, size: IconTheme.of(context).size! * 1.5),
-              ),
-              Expanded(
-                child: Text(
-                  text,
-                  textScaleFactor: 1.5,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              )
-            ],
-          )),
-    );
+    return ListTile(
+        onTap: onTap,
+        leading: Icon(icon, size: IconTheme.of(context).size! * 1.5),
+        title: Text(
+          text,
+          overflow: TextOverflow.ellipsis,
+        ));
   }
 }
